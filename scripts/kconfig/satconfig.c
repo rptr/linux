510d574d50460aa9ad08f937ef2fa4efe440a853
Patrick Franz <patfra71@gmail.com>
2018-12-28 15:47:22 +0100

    Hello satconfig

---
+++ scripts/kconfig/satconfig.c
@@ -0,0 +1,20 @@
+#define _GNU_SOURCE
+#include <assert.h>
+#include <locale.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#define LKC_DIRECT_LINK
+#include "lkc.h"
+
+int main(int argc, char *argv[])
+{
+	printf("\nHello satconfig!\n");
+	
+	return EXIT_SUCCESS;
+}
