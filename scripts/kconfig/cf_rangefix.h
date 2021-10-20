18e930f37484de3ad846f70c371c1a8d96eaab31
Patrick Franz <deltaone@debian.org>
2021-10-20 04:35:12 +0200

    Update licence info.

--- scripts/kconfig/cf_rangefix.h
+++ scripts/kconfig/cf_rangefix.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2020 Patrick Franz <patfra71@gmail.com>
+ * Copyright (C) 2021 Patrick Franz <deltaone@debian.org>
  */
 
 #ifndef CF_RANGEFIX_H
